ffc5edb90f86a5122b6d4abfa37ed10a28915ede
Potseluiko Vitalii <vetaliy063@gmail.com>
2023-08-16 15:33:11 +0300

    created Book entity, Book repository, Book service, added necessary dependencies

--- src/main/java/com/springboot/intro/IntroApplication.java
+++ src/main/java/com/springboot/intro/IntroApplication.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class IntroApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(IntroApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(IntroApplication.class, args);
+    }
 
 }
---
+++ src/main/java/com/springboot/intro/service/impl/BookServiceImpl.java
@@ -0,0 +1,24 @@
+package com.springboot.intro.service.impl;
+
+import com.springboot.intro.model.Book;
+import com.springboot.intro.repository.BookRepository;
+import com.springboot.intro.service.BookService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class BookServiceImpl implements BookService {
+    private BookRepository bookRepository;
+
+    @Override
+    public Book save(Book book) {
+        return bookRepository.save(book);
+    }
+
+    @Override
+    public List<Book> findAll() {
+        return bookRepository.findAll();
+    }
+}
