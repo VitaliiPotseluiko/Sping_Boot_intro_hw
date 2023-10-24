6ed9bde9c1643544dd66f3615f41e1c5c716b9f5
Potseluiko Vitalii <vetaliy063@gmail.com>
2023-10-24 09:43:46 +0300

    added constants to application.properties file in test directory

--- src/main/java/com/springboot/intro/security/jwt/JwtUtil.java
+++ src/main/java/com/springboot/intro/security/jwt/JwtUtil.java
@@ -4,13 +4,11 @@ import java.nio.charset.StandardCharsets;
 import java.security.Key;
 import java.util.Date;
 import java.util.function.Function;
-
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
